9421a0ddf4bc902072e9ab52e89d592cbd044bb1
Myriam Economou <myriam.economou@gmail.com>
2017-08-18 11:17:20 +0200

    Added form to contact view and dependency injection mechanism with Ninject

--- WebApplicationAngEF/Controllers/HomeController.cs
+++ WebApplicationAngEF/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationAngEF.Models;
+using WebApplicationAngEF.Services;
 
 namespace WebApplicationAngEF.Controllers
 {
     public class HomeController : Controller
     {
+        private IMailService _mailService;
+
+        public HomeController(IMailService mailService)
+        {
+            _mailService = mailService;
+
+        }
         public ActionResult Index()
         {
             return View();
@@ -24,6 +33,20 @@ namespace WebApplicationAngEF.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From: {1}{0}Email:{2}{0}Website:{3}{0}Message:{4}{0}",
+                Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+
+            if (_mailService.SendMail("noreply@yourdomain.com",
+                "foo@yourdomain.com", "Website contact", msg))
+            {
+                ViewBag.MailSent = true;
+            }
             return View();
         }
     }
